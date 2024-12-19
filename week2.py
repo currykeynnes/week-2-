8d1237651c0a0c741cb09d5462661f719fa1e03e
currykeynnes <calebwaweru03@gmail.com>
2024-12-19 18:06:07 +0300

    Create week2.py
    finished python week 2 assignment

---
+++ week2.py
@@ -0,0 +1,10 @@
+my_list = [10, 20, 30, 40,]
+my_list.insert(1, 15)
+my_list.extend([50,60, 70])
+
+my_list.pop(7)
+my_list.sort(reverse=False)
+
+index_of_30 = my_list.index (30)
+
+print("The index of 30 is:", index_of_30)
